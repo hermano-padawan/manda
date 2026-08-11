e81d3ce5e26e8535ef6f0a8d80b596301ad19dae
hermano-padawan <lluisjvi@gmail.com>
2026-08-11 22:35:29 +0200

    Print clean A4 mandalas with branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
+import "./print.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://dibulisto.site"),
